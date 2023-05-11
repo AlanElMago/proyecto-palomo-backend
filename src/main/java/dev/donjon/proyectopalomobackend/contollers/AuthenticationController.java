231875970d275a0d57b3a473003ae2b1d1ca1155
AlanElMago <alanfrancob97@gmail.com>
2023-05-10 20:25:14 -0600

    Se implementó valicación por tokens JWT
    Se agregaron archivos para la configuración básica para la autenticación
    por tokens JWT.

---
+++ src/main/java/dev/donjon/proyectopalomobackend/contollers/AuthenticationController.java
@@ -0,0 +1,42 @@
+package dev.donjon.proyectopalomobackend.contollers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.donjon.proyectopalomobackend.config.JwtUtils;
+import dev.donjon.proyectopalomobackend.dao.UserDao;
+import dev.donjon.proyectopalomobackend.dto.AuthenticationRequest;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController
+{
+    private final AuthenticationManager authenticationManager;
+    private final UserDao userDao;
+    private final JwtUtils jwtUtils;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequest request)
+    {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        final UserDetails user = userDao.findUserByEmail(request.getEmail());
+
+        if (user != null) {
+            return new ResponseEntity<String>(jwtUtils.generateToken(user), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<String>("Some error has occured.", HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/dev/donjon/proyectopalomobackend/dao/UserDao.java
@@ -0,0 +1,37 @@
+package dev.donjon.proyectopalomobackend.dao;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao
+{ 
+    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+            new User(
+                    "admin@mail.com",
+                    "admin_password",
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))),
+            new User(
+                    "user@mail.com",
+                    "user_password",
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")))
+    );
+
+    public UserDetails findUserByEmail(String email)
+    {
+        System.out.println(email);
+        return APPLICATION_USERS
+                    .stream()
+                    .filter(u -> u.getUsername().equals(email))
+                    .findFirst()
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
